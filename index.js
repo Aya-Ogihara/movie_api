087764d7f8043c0bf2d776ba5222ee912459ca0c
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-07 14:57:24 +1300

    Add myMovie info

--- index.js
+++ index.js
@@ -8,13 +8,47 @@ app.use(morgan('common'));
 //myMovie info
 let myMovie = [
   {
-    title: 'test1',
-    year: 2001
-  },  {
-    title: 'test2',
-    year: 2002
+    title: 'BREAKFAST AT TIFFANY\'S',
+    year: 1961
   },
+  {
+    title: 'ROMAN HOLIDAY',
+    year: 1953
+  }, 
+  {
+    title: 'SABRINA ',
+    year: 1954
+  },
+  {
+    title: 'CHARADE ',
+    year: 1963
+  },
+  {
+    title: 'MY FAIR LADY',
+    year: 1964
+  },
+  {
+    title: 'FUNNY FACE',
+    year: 1957
+  },
+  {
+    title: 'TWO FOR THE ROAD',
+    year: 1967
+  },
+  {
+    title: 'THE NUN’S STORY',
+    year: 1959
+  },
+  {
+    title: 'WAIT UNTIL DARK',
+    year: 1967
+  },
+  {
+    title: 'LOVE IN THE AFTERNOON ',
+    year: 1957
+  } 
 ]
+//myMovie info end 
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
