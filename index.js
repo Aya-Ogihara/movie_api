cfcf3b9fefd325508a275292d311b04aea158108
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-07 14:45:37 +1300

    Add GET routes

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-const express = require('express');
+const express = require('express'),
+morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
+
+//myMovie info
+let myMovie = [
+  {
+    title: 'test1',
+    year: 2001
+  },  {
+    title: 'test2',
+    year: 2002
+  },
+]
+
+app.get('/', (req, res) => {
+  res.send('Welcome!');
+});
+
+app.get('/movie', (req, res) => {
+  res.json(myMovie);
+});
+
+// Serving static files
+app.use(express.static('public'));
+
+// Error handling  
+app.use((err, req, res, next) => {
+  
+});
+
+// Listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.')
+});
