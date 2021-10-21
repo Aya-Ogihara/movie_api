370eecb348829ace8471393b1958fb1cecb8596e
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-22 11:26:08 +1300

    Temporary disable authentication

--- index.js
+++ index.js
@@ -52,7 +52,18 @@ app.get('/', (req, res) => {
 });
 
 // Return a list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Movies.find().populate('Genre').populate('Director').populate('Actors')
+//     .then((movies) => {
+//       res.status(201).json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send(`Error: &{err}`);
+//     });
+// });
+
+app.get('/movies',(req, res) => {
   Movies.find().populate('Genre').populate('Director').populate('Actors')
     .then((movies) => {
       res.status(201).json(movies);
@@ -62,6 +73,7 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
       res.status(500).send(`Error: &{err}`);
     });
 });
+
 
 // Return data about a single movie by title
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
