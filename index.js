0f86207e1f27e7aeadc77be48140b59ad63cc102
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-28 13:34:01 +1300

    Up date GEt user info

--- index.js
+++ index.js
@@ -213,7 +213,18 @@ app.post('/users', [
 });
 
 // Get user info
-app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+// app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Users.findOne({ Username: req.params.Username })
+//   .then((user) => {
+//     res.json(user);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send(`Error: ${err}`);
+//   })
+// });
+
+app.get('/users/:Username', (req, res) => {
   Users.findOne({ Username: req.params.Username })
   .then((user) => {
     res.json(user);
