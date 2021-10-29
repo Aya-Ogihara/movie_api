ff6ef50bb8d4674e7b3393d5dcb0ce67f8afb726
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-10-29 14:45:04 +1300

    Update user info authentication

--- index.js
+++ index.js
@@ -213,18 +213,7 @@ app.post('/users', [
 });
 
 // Get user info
-// app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-//   Users.findOne({ Username: req.params.Username })
-//   .then((user) => {
-//     res.json(user);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     res.status(500).send(`Error: ${err}`);
-//   })
-// });
-
-app.get('/users/:Username', (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOne({ Username: req.params.Username })
   .then((user) => {
     res.json(user);
